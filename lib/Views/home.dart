fdce3f1be144eda2e54796c934fa354c5da7ac2c
shatilsarower <shatild12@gmail.com>
2021-04-26 02:43:48 +0600

    8th

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:newsapp/Helper/data.dart';
 import 'package:newsapp/Model/category_MODEL.dart';
 
@@ -27,21 +28,27 @@ class _HomeState extends State<Home> {
         ),
         elevation: 5.0,
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    return CategoryTile(
-                      imageUrl: categories[index].imageUrl,
-                      categoryName: categories[index].categoryName,
-                    );
-                  }),
-            )
-          ],
+      body: GestureDetector(
+        onTap: () {},
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                height: 70,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: categories.length,
+                    itemBuilder: (context, index) {
+                      return CategoryTile(
+                        imageUrl: categories[index].imageUrl,
+                        categoryName: categories[index].categoryName,
+                      );
+                    }),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -57,11 +64,46 @@ class CategoryTile extends StatelessWidget {
     return Container(
       child: Stack(
         children: [
-          Image.network(
-            imageUrl,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.network(
+              imageUrl,
+              width: 120,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.black45,
+              borderRadius: BorderRadius.circular(6),
+            ),
             width: 120,
             height: 60,
-          ),
+            child: Text(
+              categoryName,
+              style: TextStyle(color: Colors.white),
+            ),
+          ) //Text
+        ],
+      ),
+    );
+  }
+}
+
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc;
+  BlogTile(
+      {@required this.imageUrl, @required this.title, @required this.desc});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(imageUrl),
+          Text(title),
+          Text(desc),
         ],
       ),
     );
