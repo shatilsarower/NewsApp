dd9234d0d8e2463ee78554567d2f0ca9a80cd738
sms <shatild12@gmail.com>
2021-06-08 23:09:50 +0600

    15th

---
+++ lib/Views/article_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class Article_View extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
