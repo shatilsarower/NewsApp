256f8fa7cb44bffc061573973032e29f65ff98e2
sms <shatild12@gmail.com>
2021-06-09 00:12:45 +0600

    18th

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -8,6 +8,17 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 4.0,
+        title: Text(
+          "NEWS",
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
   }
 }
