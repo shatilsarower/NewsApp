5b7f9585257d3e9bd90663096825bc1a47926e5d
sms <shatild12@gmail.com>
2021-06-11 00:02:15 +0600

    26th

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -59,35 +59,40 @@ class CategoryTile extends StatelessWidget {
   CategoryTile({this.imageUrl, this.categoryName});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(right: 16),
-      child: Stack(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(6),
-            child: Image.network(
-              imageUrl,
-              width: 120,
-              height: 60,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.black26,
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.only(right: 16),
+        child: Stack(
+          children: [
+            ClipRRect(
               borderRadius: BorderRadius.circular(6),
-            ),
-            width: 120,
-            height: 60,
-            child: Text(
-              categoryName,
-              style: TextStyle(
-                color: Colors.white,
+              child: Image.network(
+                imageUrl,
+                width: 120,
+                height: 60,
+                fit: BoxFit.cover,
               ),
             ),
-          ),
-        ],
+            Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.black26,
+                borderRadius: BorderRadius.circular(6),
+              ),
+              width: 120,
+              height: 60,
+              child: Text(
+                categoryName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
